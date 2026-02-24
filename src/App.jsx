2f04a3861740a3e9581bdfebb616869d6ba0003e
gyut76776 <Bidhen86@gmail.com>
2026-02-24 22:54:53 +0700

    fix: case sensitive import

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import AdminLayout from "./layouts/AdminLayout";
 import AdminRoute from "./Admin/routes/AdminRoute";
 
 import Pesanan from "./Admin/pages/Pesanan";
-import Menu from "./Admin/pages/Menu";
+import Menu from "./Admin/pages/menu";
 import Pelanggan from "./Admin/pages/Pelanggan";
-import Barista from "./Admin/pages/barista";
+import Barista from "./Admin/pages/Barista";
 import Kitchen from "./Admin/pages/Kitchen";
 import Login from "./Admin/pages/Login";
 import AdminDashboard from "./Admin/pages/Dashboard";
